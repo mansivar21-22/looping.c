003bbafaea3197e92d22b3cbbcf29a144a51dad3
Mansi Varshney <70686638+mansivar21-22@users.noreply.github.com>
2021-01-06 22:53:27 +0530

    Add files via upload

---
+++ looping.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,n;
+    printf("\nThe square of n natural numbers are:-\n");
+    for(a=1;a<=n;a++)
+    {
+        printf("%d",a*a);
+        printf("\n");
+    }
+}
